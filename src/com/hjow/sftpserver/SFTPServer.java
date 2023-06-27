3b9b69db0012c6e31f29d94c52e2da5f93b77e3a
HJOW <hujinone@trionsoft.com>
2023-06-27 15:22:42 +0900

    경고문구 추가

--- src/com/hjow/sftpserver/SFTPServer.java
+++ src/com/hjow/sftpserver/SFTPServer.java
@@ -1,3 +1,20 @@
+/*
+This project is just simple implementation of Apache MINA FTP server.
+
+Copyright 2023 HJOW
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package com.hjow.sftpserver;
 
 import java.io.IOException;
@@ -16,6 +33,13 @@ import org.apache.sshd.server.session.ServerSession;
 
 import com.hjow.ftpserver.LiteUserManager;
 
+/**
+ * Caution ! None-completed source !!
+ * 
+ * 
+ * @author HJOW
+ *
+ */
 public class SFTPServer implements AutoCloseable {
     protected SshServer ssh;
     protected LiteUserManager userManager;
